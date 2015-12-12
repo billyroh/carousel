c8175c38e413a6f15218f785d5ac13da0b7e3e81
Billy Roh <billy.roh@gmail.com>
2015-12-11 17:15:50 -0800

    it's working!!

--- script.js
+++ script.js
@@ -1,81 +1,166 @@
-angular.module('carousel', [])
-  .run(function($rootScope) {
-    numberOfPhotos = 20
-    maxWidth = 50
-    minWidth = 10
-    $rootScope.images = []
+// Variables
+var width = 1000,
+    thumbnailHeight = 50,
+    thumbnailWidth = 50,
+    thumbnailMinimumWidth = 5;
+    dataset = [],
+    numberOfPhotos = 20;
 
-    // Fill up the thumbnails
-    for(var index = 0; index < numberOfPhotos; index++) {
-      $rootScope.images.push("images/photo-" + index + ".jpg")
-    }
+// TODO
+// Set up an x axis
+var scale = d3.fisheye.scale(d3.scale.linear)
+                    .domain([0, numberOfPhotos])
+                    .range([0, width - thumbnailMinimumWidth])
 
-    $rootScope.handleCursor = function(index, event) {
-      this.updatePreview(index);
-      this.useCoordinates(index, event);
-    }
+// Place one image on each x axis tick
+// Expand and contract the axis, which handles the image rendering
 
-    // Update the preview with the proper image
-    $rootScope.updatePreview = function(index) {
-      preview = document.getElementById('preview')
-      photoSource = 'images/photo-' + index + '.jpg'
-      preview.style.backgroundImage = 'url("' + photoSource + '")'
-    }
+// Fisheye stuff
+var xFisheye = d3.fisheye.scale(d3.scale.identity).domain([0, width]).focus(width / 2);
 
-    $rootScope.useCoordinates = function(index, event) {
-      cursorX = event.screenX
-      element = document.getElementById('photo-' + index)
-      element.style.width = maxWidth
-      adjacentDepth = 3
+// Set up array
+for (var i = 0; i < numberOfPhotos; i++) {
+  string = 'images/photo-' + i + '.jpg';
+  dataset.push(string);
+}
 
-      for (var adjacentIndex = 1; adjacentIndex <= adjacentDepth; adjacentIndex++) {
-        if ((index - adjacentDepth) >= 0) {
-          this.checkLeftElement(index, cursorX, adjacentIndex)
-        }
-        if ((index + adjacentDepth) <= numberOfPhotos - 1) {
-          this.checkRightElement(index, cursorX, adjacentIndex)
-        }
-      }
-    }
+// Set up list
+var svg = d3.select('#list').append('svg')
+      .attr('width', width)
+      .attr('height', thumbnailHeight)
+      .attr('fill', 'red')
 
-    $rootScope.checkLeftElement = function(index, cursorX, adjacentIndex) {
-      // Select the left-adjacent element
-      element = document.getElementById('photo-' + (index - adjacentIndex))
+// Display thumbnails
+var thumbnail = svg.selectAll('image')
+  .data(dataset)
+  .enter()
+  .append('svg:image')
+    .attr('class', 'thumbnail')
+    .attr('width', function(d, i) { return getWidth(i); })
+    .attr('height', thumbnailHeight)
+    .attr('x', function(d, i) { return scale(i) })
+    .attr('preserveAspectRatio', 'xMinYMin slice')
+    .attr('xlink:href', function(d) { return d });
 
-      // Calculate the distance between the cursor at the right edge of the element.
-      distance = cursorX - element.getBoundingClientRect().right
+svg.on('mousemove', function () {
+  var mouse = d3.mouse(this);
+  scale.distortion(2.5).focus(mouse[0]);
+  thumbnail.call(position)
+})
 
-      // Scale the element's width based on that distance.
-      percentage = 1 - Math.abs(distance / maxWidth)
-      desiredWidth =
-      console.log("index: " + adjacentIndex)
-      console.log("width: " + percentage * maxWidth)
-      console.log("thing: " + Math.abs(distance / maxWidth))
-      console.log("")
-      if (percentage * maxWidth < minWidth) {
-        element.style.width = minWidth
-      } else {
-        element.style.width = percentage * maxWidth
-      }
-    }
+function getWidth(i) {
+  if (i === (numberOfPhotos - 1)) {
+    return thumbnailMinimumWidth
+  } else {
+    return scale(i + 1) - scale(i);
+  }
+}
 
-    $rootScope.checkRightElement = function(index, cursorX, adjacentIndex) {
-      // Select the right-adjacent element
-      element = document.getElementById('photo-' + (index + adjacentIndex))
+function position(thumb) {
+  thumb .attr('x', function(d, i) { return scale(i) })
+        .attr('width', function(d , i) { return getWidth(i) })
+}
 
-      // Calculate the distance between the cursor at the left edge of the element.
-      distance = element.getBoundingClientRect().left - cursorX
 
-      // Scale the element's padding based on that distance.
-      percentage = 1 - Math.abs(distance / maxWidth)
-      if (percentage * maxWidth < minWidth) {
-        element.style.width = minWidth
-      } else {
-        element.style.width = percentage * maxWidth
-      }
-    }
 
-    $rootScope.handleMouseLeave = function(index) {
-      return
-    }
-  })
+// (function chart4() {
+//
+//   // Various accessors that specify the four dimensions of data to visualize.
+//   function x(d) { return d.income; }
+//   function y(d) { return d.lifeExpectancy; }
+//   function radius(d) { return d.population; }
+//   function color(d) { return d.region; }
+//
+//   // Chart dimensions.
+//   var margin = {top: 5.5, right: 19.5, bottom: 12.5, left: 39.5},
+//       width = 960,
+//       height = 500 - margin.top - margin.bottom;
+//
+//   // Various scales and distortions.
+//   var xScale = d3.fisheye.scale(d3.scale.log).domain([300, 1e5]).range([0, width]),
+//       yScale = d3.fisheye.scale(d3.scale.linear).domain([20, 90]).range([height, 0]),
+//       radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
+//       colorScale = d3.scale.category10().domain(["Sub-Saharan Africa", "South Asia", "Middle East & North Africa", "America", "Europe & Central Asia", "East Asia & Pacific"]);
+//
+//   // The x & y axes.
+//   var xAxis = d3.svg.axis().orient("bottom").scale(xScale).tickFormat(d3.format(",d")).tickSize(-height),
+//       yAxis = d3.svg.axis().scale(yScale).orient("left").tickSize(-width);
+//
+//   // Create the SVG container and set the origin.
+//   var svg = d3.select("#chart4").append("svg")
+//       .attr("width", width + margin.left + margin.right)
+//       .attr("height", height + margin.top + margin.bottom)
+//     .append("g")
+//       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//   // Add a background rect for mousemove.
+//   svg.append("rect")
+//       .attr("class", "background")
+//       .attr("width", width)
+//       .attr("height", height);
+//
+//   // Add the x-axis.
+//   svg.append("g")
+//       .attr("class", "x axis")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(xAxis);
+//
+//   // Add the y-axis.
+//   svg.append("g")
+//       .attr("class", "y axis")
+//       .call(yAxis);
+//
+//   // Add an x-axis label.
+//   svg.append("text")
+//       .attr("class", "x label")
+//       .attr("text-anchor", "end")
+//       .attr("x", width - 6)
+//       .attr("y", height - 6)
+//       .text("income per capita, inflation-adjusted (dollars)");
+//
+//   // Add a y-axis label.
+//   svg.append("text")
+//       .attr("class", "y label")
+//       .attr("text-anchor", "end")
+//       .attr("x", -6)
+//       .attr("y", 6)
+//       .attr("dy", ".75em")
+//       .attr("transform", "rotate(-90)")
+//       .text("life expectancy (years)");
+//
+//   // Load the data.
+//   d3.json("nations.json", function(nations) {
+//
+//     // Add a dot per nation. Initialize the data at 1800, and set the colors.
+//     var dot = svg.append("g")
+//         .attr("class", "dots")
+//       .selectAll(".dot")
+//         .data(nations)
+//       .enter().append("circle")
+//         .attr("class", "dot")
+//         .style("fill", function(d) { return colorScale(color(d)); })
+//         .call(position)
+//         .sort(function(a, b) { return radius(b) - radius(a); });
+//
+//     // Add a title.
+//     dot.append("title")
+//         .text(function(d) { return d.name; });
+//
+//     // Positions the dots based on data.
+//     function position(dot) {
+//       dot .attr("cx", function(d) { return xScale(x(d)); })
+//           .attr("cy", function(d) { return yScale(y(d)); })
+//           .attr("r", function(d) { return radiusScale(radius(d)); });
+//     }
+//
+//     svg.on("mousemove", function() {
+//       var mouse = d3.mouse(this);
+//       xScale.distortion(2.5).focus(mouse[0]);
+//       yScale.distortion(2.5).focus(mouse[1]);
+//
+//       dot.call(position);
+//       svg.select(".x.axis").call(xAxis);
+//       svg.select(".y.axis").call(yAxis);
+//     });
+//   });
+// })();
